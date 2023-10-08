35a1f02715c7573958fb90638b0c1b573d8955b8
Ahmedkel <ahmedkeloch@gmail.com>
2023-10-08 21:56:10 +0100

    trying to run it locally

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """Script to deploy web static"""
 
-from fabric.api import put, run, task, env
+from fabric.api import local, put, run, task, env
 from os.path import exists, splitext
 
 # Define target hosts
 env.hosts = ['100.25.142.90', '54.145.241.45']
+
+@task
+def do_pack():
+    """Archives the web_static folder."""
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(date)
+    print("Packing web_static to {}".format(path))
+    if local('{} && tar -cvzf {} web_static'.format(mkdir, path)).succeeded:
+        size = os.path.getsize(path)
+        print("web_static packed: {} -> {}Bytes".format(path, size))
+        return path
+    return None
 
 
 @task
