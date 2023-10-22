d2bdb00c419cdd0ad9c912323dc549e5404cbe21
Ahmedkel <ahmedkeloch@gmail.com>
2023-10-22 11:34:38 +0100

    List of cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application. """
+
+from flask import Flask, render_template
+from models import *
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ HTML page with list of states sorted by name """
+    states = sorted(storage.all(State).values(), key=lambda i: i.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    """ Remove SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
